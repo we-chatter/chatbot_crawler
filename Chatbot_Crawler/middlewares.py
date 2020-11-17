154c8a34997932d07fcde3cc91d2f762f8fe09d9
xu <charlesxu86@163.com>
2020-11-17 22:30:20 +0800

    Initial commit

---
+++ Chatbot_Crawler/middlewares.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+import random
+import logging
+import os
+import redis
+import json
+
+from cookies
+from scrapy import signals
+
+
+logger = logging.getLogger(__name__)
+
+class ChatbotCrawlerSpiderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, dict or Item objects.
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Request, dict
+        # or Item objects.
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class ChatbotCrawlerDownloaderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+        return None
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+class UserAgentMiddleware(object):
+    """ 换User-Agent """
+
+    def process_request(self, request, spider):
+        agent = random.choice(agents)
+        request.headers["User-Agent"] = agent
+
+class CookiesMiddleware(RetryMiddleware):
+    """ 维护Cookie """
+
+    def __init__(self, settings, crawler):
+        RetryMiddleware.__init__(self, settings)
+        self.rconn = settings.get("RCONN", redis.Redis(crawler.settings.get('REDIS_HOST', 'localhsot'), crawler.settings.get('REDIS_PORT', 6379)))
+        initCookie(self.rconn, crawler.spider.name)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings, crawler)
+
+    def process_request(self, request, spider):
+        redisKeys = self.rconn.keys()
+        while len(redisKeys) > 0:
+            elem = random.choice(redisKeys)
+            if "SinaSpider:Cookies" in elem:
+                cookie = json.loads(self.rconn.get(elem))
+                request.cookies = cookie
+                request.meta["accountText"] = elem.split("Cookies:")[-1]
+                break
+            else:
+                redisKeys.remove(elem)
+
+    def process_response(self, request, response, spider):
+        if response.status in [300, 301, 302, 303]:
+            try:
+                redirect_url = response.headers["location"]
+                if "login.weibo" in redirect_url or "login.sina" in redirect_url:  # Cookie失效
+                    logger.warning("One Cookie need to be updating...")
+                    updateCookie(request.meta['accountText'], self.rconn, spider.name)
+                elif "weibo.cn/security" in redirect_url:  # 账号被限
+                    logger.warning("One Account is locked! Remove it!")
+                    removeCookie(request.meta["accountText"], self.rconn, spider.name)
+                elif "weibo.cn/pub" in redirect_url:
+                    logger.warning(
+                        "Redirect to 'http://weibo.cn/pub'!( Account:%s )" % request.meta["accountText"].split("--")[0])
+                reason = response_status_message(response.status)
+                return self._retry(request, reason, spider) or response  # 重试
+            except Exception as e:
+                raise IgnoreRequest
+        elif response.status in [403, 414]:
+            logger.error("%s! Stopping..." % response.status)
+            os.system("pause")
+        else:
+            return response
